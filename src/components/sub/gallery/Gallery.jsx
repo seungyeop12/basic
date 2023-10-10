4e3c37d5c9c66f633c1b71e6a688e174dfb5946c
seungyeop12 <art7449@gmail.com>
2023-10-10 10:58:33 +0900

    data fetching함수 async await로 개선

--- src/components/sub/gallery/Gallery.jsx
+++ src/components/sub/gallery/Gallery.jsx
@@ -1,36 +1,63 @@
 import Layout from '../../common/layout/Layout';
 import './Gallery.scss';
 import { useState, useEffect } from 'react';
+import Masonry from 'react-masonry-component';
 
 export default function Gallery() {
 	const [Pics, setPics] = useState([]);
-	const api_key = '9283e96df728a0ba4e1f9b9bdc9ae785';
-	const method_interest = 'flickr.interestingness.getList';
-	const num = 500;
-	const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
+
+	const fetchData = async () => {
+		const api_key = '9283e96df728a0ba4e1f9b9bdc9ae785';
+		const method_interest = 'flickr.interestingness.getList';
+		const num = 50;
+		const url = `https://www.flickr.com/services/rest/?method=${method_interest}&api_key=${api_key}&per_page=${num}&nojsoncallback=1&format=json`;
+
+		//만약 특정함수가 promise를 반환한다면 warpping함수로 묶어준뒤 async 지정
+		//각각의 promise 반환 함수 앞쪽에 await를 붙이기만 하면 해당 코드는 동기화됨
+		//지저분하게 depth를 들여쓰기 해가면서 then구문을 호출할 필요가 없음
+		const data = await fetch(url);
+		const json = await data.json();
+		console.log(json.photos.photo);
+		setPics(json.photos.photo);
+	};
 
 	useEffect(() => {
-		fetch(url)
-			.then((data) => data.json())
-			.then((json) => {
-				console.log(json.photos.photo);
-				setPics(json.photos.photo);
-			});
+		fetchData();
 	}, []);
 
 	return (
 		<Layout title={'Gallery'}>
-			{Pics.map((data, idx) => {
-				return (
-					<article key={idx}>
-						<h2>{data.title}</h2>
-						<img
-							src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
-							alt={data.title}
-						/>
-					</article>
-				);
-			})}
+			<div className='picFrame'>
+				<Masonry
+					elementType={'div'}
+					options={{ transitionDuration: '0.5s' }}
+					disableImagesLoaded={false}
+					updateOnEachImageLoad={false}
+				>
+					{Pics.map((data, idx) => {
+						return (
+							<article key={idx}>
+								<div className='inner'>
+									<img
+										className='pic'
+										src={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_m.jpg`}
+										alt={`https://live.staticflickr.com/${data.server}/${data.id}_${data.secret}_b.jpg`}
+									/>
+									<h2>{data.title}</h2>
+
+									<div className='profile'>
+										<img
+											src={`http://farm${data.farm}.staticflickr.com/${data.server}/buddyicons/${data.owner}.jpg`}
+											alt={data.owner}
+										/>
+										<span>{data.owner}</span>
+									</div>
+								</div>
+							</article>
+						);
+					})}
+				</Masonry>
+			</div>
 		</Layout>
 	);
 }
