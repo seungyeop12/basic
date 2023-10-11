b00e434451e3d4d959b01bbf620e45417875e4a2
seungyeop12 <art7449@gmail.com>
2023-10-11 16:08:23 +0900

    member 페이지 완성본

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -2,31 +2,22 @@ import Layout from '../../common/layout/Layout';
 import Modal from '../../common/modal/Modal';
 import './Youtube.scss';
 import { useEffect, useState } from 'react';
-/*
-  리액트는 단방향 데이터 바인딩
-  - 부모에서 자식으로 데이터 전달가능하지만 자식에서 부모로는 데이터를 전달 불가
-  - props전달, children 전달
- 
-  - 리액트에서 자식 컴포넌트에서는 직접적으로 부모 컴포넌트의 state값 변경이 불가
-    - 해결방법 부모의 state변경함수를 자식 컴포넌트로 전달
-    - 자식컴포넌트에서는 전달받은 state변경함수로 간접적으로 부모 state값 변경가능
-*/
+
 export default function Youtube() {
 	const [Youtube, setYoutube] = useState([]);
 	const [IsModal, setIsModal] = useState(false);
 	const [Index, setIndex] = useState(0);
-	const fetchYoutube = () => {
+
+	const fetchYoutube = async () => {
 		const api_key = 'AIzaSyA4iVjYFWaYcsCq0z7a2Ey2FO9MTtXA6QI';
 		const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
 		const pid = 'PLMMMmfSQpakSd6WsuPqYUflXHqd1YJliN';
 		const num = 10;
 		const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
-		fetch(resultURL)
-			.then((data) => data.json())
-			.then((json) => {
-				console.log(json);
-				setYoutube(json.items);
-			});
+
+		const data = await fetch(resultURL);
+		const json = await data.json();
+		setYoutube(json.items);
 	};
 	useEffect(() => {
 		fetchYoutube();
@@ -67,3 +58,19 @@ export default function Youtube() {
 		</>
 	);
 }
+/*
+  리액트는 단방향 데이터 바인딩
+  - 부모에서 자식으로 데이터 전달가능하지만 자식에서 부모로는 데이터를 전달 불가
+  - props전달, children 전달
+ 
+  - 리액트에서 자식 컴포넌트에서는 직접적으로 부모 컴포넌트의 state값 변경이 불가
+    - 해결방법 부모의 state변경함수를 자식 컴포넌트로 전달
+    - 자식컴포넌트에서는 전달받은 state변경함수로 간접적으로 부모 state값 변경가능
+
+	promise .then구문을 좀 더 구조적으로 짜는 방법 (async await) -> then구문을 쓸 필요가 없어짐
+	async await 사용조건
+	- promise반환함수를 wrapping 처리
+	- wrapping 된 함수에 async 적용
+	- promise반환함수 앞쪽에 await 적용
+	- await 적용되어 있는 다음 코드가 무조건 동기화 처리
+*/
