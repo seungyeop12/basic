1606c09e840a3bb2dacf72e47cef45cfc9089498
seungyeop12 <art7449@gmail.com>
2023-10-11 23:21:32 +0900

    Youtube 50% 완성

--- src/components/sub/youtube/Youtube.jsx
+++ src/components/sub/youtube/Youtube.jsx
@@ -12,7 +12,7 @@ export default function Youtube() {
 		const api_key = 'AIzaSyA4iVjYFWaYcsCq0z7a2Ey2FO9MTtXA6QI';
 		const baseURL = 'https://www.googleapis.com/youtube/v3/playlistItems';
 		const pid = 'PLMMMmfSQpakSd6WsuPqYUflXHqd1YJliN';
-		const num = 10;
+		const num = 5;
 		const resultURL = `${baseURL}?key=${api_key}&part=snippet&playlistId=${pid}&maxResults=${num}`;
 
 		const data = await fetch(resultURL);
