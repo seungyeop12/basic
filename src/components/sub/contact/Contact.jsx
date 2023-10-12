ebe1a9f566ca414f3c640a32f023aec261532c2a
seungyeop12 <art7449@gmail.com>
2023-10-12 16:45:54 +0900

    contact 가르기

--- src/components/sub/contact/Contact.jsx
+++ src/components/sub/contact/Contact.jsx
@@ -148,39 +148,40 @@ export default function Contact() {
 
 	return (
 		<Layout title={'Contact'}>
-			<div className='ha'>
-				<h2>Email: art7449@gmail.com</h2>
-				<h2>Tel: 010-7449-8049</h2>
-				<h2>Name: Cha Seung Yeop</h2>
-			</div>
-			<div className='ck'>
-				<h2 className='ta'>Lorem, ipsum.</h2>
-				<p>
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Doloribus nemo vel
-					necessitatibus!
-				</p>
-			</div>
-			<div id='mailBox'>
-				<form ref={form} onSubmit={sendEmail}>
-					<div className='upper'>
-						<label>Name</label>
-						<input type='text' name='user_name' className='nameEl' />
-						<label>Email</label>
-						<input type='email' name='user_email' className='emailEl' />
-					</div>
+			<div className='upperbox'>
+				<div id='mailBox'>
+					<form ref={form} onSubmit={sendEmail}>
+						<div className='upper'>
+							<label>Name</label>
+							<input type='text' name='user_name' className='nameEl' />
+							<label>Email</label>
+							<input type='email' name='user_email' className='emailEl' />
+						</div>
 
-					<div className='lower'>
-						<label>Message</label>
-						<textarea name='message' className='msgEl' />
-					</div>
+						<div className='lower'>
+							<label>Message</label>
+							<textarea name='message' className='msgEl' />
+						</div>
 
-					<div className='btnSet'>
-						<input type='reset' value='Cancel' />
-						<input type='submit' value='Send' />
-					</div>
-				</form>
+						<div className='btnSet'>
+							<input type='reset' value='Cancel' />
+							<input type='submit' value='Send' />
+						</div>
+					</form>
+				</div>
+				<div className='ha'>
+					<h2>Email: art7449@gmail.com</h2>
+					<h2>Tel: 010-7449-8049</h2>
+					<h2>Name: Cha Seung Yeop</h2>
+				</div>
+				<div className='ck'>
+					<h2 className='ta'>Lorem, ipsum.</h2>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. <br /> Doloribus nemo vel
+						necessitatibus!
+					</p>
+				</div>
 			</div>
-
 			<div id='mapBox'>
 				<button onClick={() => setTraffic(!Traffic)}>
 					{Traffic ? '교통정보 끄기' : '교통정보 켜기'}
