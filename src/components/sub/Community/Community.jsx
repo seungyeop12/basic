ae773e5593236f3831bcf4cc8cde7051c827ce3b
seungyeop12 <art7449@gmail.com>
2023-10-13 10:14:31 +0900

    기본 게시글 UI생성

--- src/components/sub/Community/Community.jsx
+++ src/components/sub/Community/Community.jsx
@@ -1,23 +1,57 @@
-import { useRef } from 'react';
 import Layout from '../../common/layout/Layout';
 import './Community.scss';
+import { useRef, useState } from 'react';
 
 export default function Community() {
 	const refInput = useRef(null);
 	const refTextarea = useRef(null);
+	const [Posts, setPosts] = useState([]);
+
+	const resetForm = () => {
+		refInput.current.value = '';
+		refTextarea.current.value = '';
+	};
+	const createPost = () => {
+		if (!refInput.current.value.trim() || !refTextarea.current.value.trim()) {
+			resetForm();
+			return alert('제목과 본문을 모두 입력하세요.');
+		}
+		//기존 Posts 배열값을 Deep copy해서 가져온뒤, 그 뒤에 추가로 방금 입력한 객체를 배열에 추가
+		setPosts([{ title: refInput.current.value, content: refTextarea.current.value }, ...Posts]);
+	};
 
 	return (
-		<Layout title={'Community'}>
+		<Layout title={'Communtiy'}>
 			<div className='inputBox'>
-				<input ref={refInput} type='type' placeholder='제목을 입력하세요' />
+				<input ref={refInput} type='text' placeholder='제목을 입력하세요.' />
 				<br />
-				<textarea ref={refTextarea} cols='' rows='' placeholder='본문을 입력하세요'></textarea>
+				<textarea ref={refTextarea} cols='30' rows='3' placeholder='본문을 입력하세요.'></textarea>
 
 				<nav className='btnSet'>
-					<button>cancle</button>
-					<button>write</button>
+					<button onClick={resetForm}>cancel</button>
+					<button onClick={createPost}>write</button>
 				</nav>
+			</div>
+
+			<div className='showBox'>
+				{Posts.map((post, idx) => {
+					return (
+						<article key={idx}>
+							<h2>{post.title}</h2>
+							<p>{post.content}</p>
+						</article>
+					);
+				})}
 			</div>
 		</Layout>
 	);
 }
+
+/*
+  Create : 게시글 저장
+  Read : 게시글 보기
+  Update : 게시글 수정
+  Delete : 게시글 삭제
+
+  localStorage : 모든 브라우저가 가지고 있는 경량의 저장소 (문자열: 5MB)
+*/
