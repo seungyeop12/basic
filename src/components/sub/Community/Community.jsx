d2aab4de04cbf8e7f68b47cf982753a7de8cc100
seungyeop12 <art7449@gmail.com>
2023-10-13 10:15:23 +0900

    삭제기능 구현

--- src/components/sub/Community/Community.jsx
+++ src/components/sub/Community/Community.jsx
@@ -18,6 +18,12 @@ export default function Community() {
 		}
 		//기존 Posts 배열값을 Deep copy해서 가져온뒤, 그 뒤에 추가로 방금 입력한 객체를 배열에 추가
 		setPosts([{ title: refInput.current.value, content: refTextarea.current.value }, ...Posts]);
+		resetForm();
+	};
+
+	const deletePost = (delIndex) => {
+		//기존 Posts배열을 반복 돌면서 인수로 전달된 삭제 순번값과 현재 반복되는 배열의 순번값이 같지 않은 것만 리턴
+		setPosts(Posts.filter((_, idx) => delIndex !== idx));
 	};
 
 	return (
@@ -39,6 +45,11 @@ export default function Community() {
 						<article key={idx}>
 							<h2>{post.title}</h2>
 							<p>{post.content}</p>
+
+							<nav className='btnSet'>
+								<button>Edit</button>
+								<button onClick={() => deletePost(idx)}>Delete</button>
+							</nav>
 						</article>
 					);
 				})}
