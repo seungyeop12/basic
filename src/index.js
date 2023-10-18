77eb6811a6120d47960d9a9f3134e229abcab787
seungyeop12 <art7449@gmail.com>
2023-10-18 10:50:02 +0900

    modal 관련 client-side-data전역관리

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import youtubrReducer from './redux/youtubeSlice';
 import flickrReducer from './redux/flickrSlice';
+import modalReducer from './redux/modalSlice';
 
 const store = configureStore({
 	reducer: {
 		youtube: youtubrReducer,
 		flickr: flickrReducer,
+		modal: modalReducer,
 	},
 });
 
---
+++ src/redux/modalSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const modalSlice = createSlice({
+	name: 'modal',
+	initialState: { open: false },
+	reducers: {
+		open: (state) => (state.open = true),
+		close: (state) => (state.open = false),
+	},
+});
+
+export const { open, close } = modalSlice.actions;
+export default modalSlice.reducer;
