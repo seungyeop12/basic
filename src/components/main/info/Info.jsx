9d9a3f558a5d48661dc667a6752f59b8f98eb7c1
seungyeop12 <art7449@gmail.com>
2023-10-20 08:14:47 +0900

    Btns 컴포넌트 추가 및 스타일링

--- src/components/main/info/Info.jsx
+++ src/components/main/info/Info.jsx
@@ -3,7 +3,7 @@ import './Info.scss';
 function Info() {
 	const { data } = useSelector((store) => store.flickr);
 	return (
-		<section className='info'>
+		<section className='myScroll'>
 			<div className='wrap'>
 				{data.map((pic, idx) => {
 					if (idx >= 6) return null;
