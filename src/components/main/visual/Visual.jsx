e9270580e76cad793f786dd199150502e2045a17
seungyeop12 <art7449@gmail.com>
2023-10-19 11:18:07 +0900

    버튼 활성화 모션

--- src/components/main/visual/Visual.jsx
+++ src/components/main/visual/Visual.jsx
@@ -1,19 +1,25 @@
 import './Visual.scss';
-import { useSelector } from 'react-redux/es/hooks/useSelector';
+import { useSelector } from 'react-redux';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 
 function Visual() {
 	const { data } = useSelector((store) => store.youtube);
+	console.log(data);
+
 	return (
 		<section className='visual'>
-			<Swiper>
+			<Swiper slidesPerView={3} spaceBetween={50} loop={true} centeredSlides={true}>
 				{data.map((vid, idx) => {
 					if (idx >= 5) return null;
 					return (
 						<SwiperSlide key={idx}>
 							<div className='inner'>
+								<div className='pic'>
+									<img src={vid.snippet.thumbnails.maxres.url} alt={vid.title} />
+								</div>
 								<h2>{vid.snippet.title}</h2>
+								<button>VIEW</button>
 							</div>
 						</SwiperSlide>
 					);
