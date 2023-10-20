c762c493ecc6272a9124d906fa6498a310ae7e4e
seungyeop12 <art7449@gmail.com>
2023-10-20 15:07:30 +0900

    갤러리 디자인

--- src/components/main/info/Info.jsx
+++ src/components/main/info/Info.jsx
@@ -3,16 +3,18 @@ import './Info.scss';
 function Info() {
 	const { data } = useSelector((store) => store.flickr);
 	return (
-		<section className='myScroll'>
+		<section className='Info'>
 			<div className='wrap'>
 				{data.map((pic, idx) => {
 					if (idx >= 6) return null;
 					return (
 						<article key={idx}>
-							<img
-								src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
-								alt={pic.title}
-							/>
+							<div className='inner'>
+								<img
+									src={`https://live.staticflickr.com/${pic.server}/${pic.id}_${pic.secret}_b.jpg`}
+									alt={pic.title}
+								/>
+							</div>
 						</article>
 					);
 				})}
